4c3005ac8fedf6838c053035508f3e7da085523c
Charlie Rix <charlie.rix@gmail.com>
2022-07-24 18:32:51 -0500

    Bumped from .net core 3.1 to 6.0-windows

---
+++ Math_WPF/WPF/Controls3D/DebugLogWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Game.Math_WPF.WPF.Controls3D
+{
+    /// <summary>
+    /// This views 3D objects defined in a json file that was built from in game
+    /// </summary>
+    /// <remarks>
+    /// Models\LogScene is the root filetype
+    /// 
+    /// See txtFile_TextChanged
+    /// 
+    /// A lot of this code is just copied from Debug3DWindow and UtilityWPF
+    /// </remarks>
+    public partial class DebugLogWindow : Window
+    {
+        public DebugLogWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
